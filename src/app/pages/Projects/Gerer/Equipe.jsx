e520c9ecf27ab57246a2f9539615af9f38ea9c6f
Corso Rémi <remi.corso@hotmail.fr>
2020-01-17 00:26:10 +0100

    Petit Commit

--- src/app/pages/Projects/Gerer/Equipe.jsx
+++ src/app/pages/Projects/Gerer/Equipe.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Import Icons
 import {FaUsersCog} from "react-icons/fa";
 
-class Equipe extends React.Component {
-  state = {};
-  render() {
+const Equipe = ({ checkUserStatus: isOnline, onLogoutUser: onLogout }) => {
+
+  let response = [
+    {id: 1, username: 'remi'},
+    {id: 2, username: 'jordan'}
+  ];
+
+  const [teammateTab, setteammateTab] = useState(response);
+
+
+
     return (
       <div className="h-100 d-flex flex-column ">
       <h2 className="m-2 text-center"><FaUsersCog /> Équipe</h2>
+      <div className="">
+      <ul>
+      {teammateTab.map(teammate => { return <li> {teammate.username} </li> })}
+        </ul>
+        </div>
       </div>
     );
-  }
+  
 }
 
 export default Equipe;
