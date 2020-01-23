33cef2b479167a0e243011d3cf9845de4d403e66
Guillaume <guillaume.cirillo@gmail.com>
2020-01-23 15:21:42 +0100

    patch 0.6

--- src/app/pages/Home/HomeProjectList.jsx
+++ src/app/pages/Home/HomeProjectList.jsx
@@ -20,8 +20,8 @@ function HomeProjectList(){
     }, []);
 
     const listProject = projects.map((project) =>
-    <Link to={`projet/${project._id}`} handler={ProjetSeul}>
-    <div className="single-list-result block-dark-hover pointer py-2 px-4">
+    <div className="single-list-result block-dark-hover pointer py-2 px-4 ">
+           <Link to={`projet/${project._id}`} handler={ProjetSeul}>
     <div className="row mx-0">
         <div className="d-flex justify-content-center col-2">
             <GoTasklist />
@@ -33,8 +33,8 @@ function HomeProjectList(){
     <div className="row mx-0 text-center">
     <p>{project.description}</p>
         </div>
-        </div>
        </Link>
+        </div>
          );
 
         return (
